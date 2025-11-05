4fcca15e2985c488b40232747d4ff749bdac664c
Emilie Sengenbjerg <Emilie_Sengenbjerg@hotmail.com>
2025-11-05 09:55:04 +0100

    Removed comments for work structure

--- index.php
+++ index.php
@@ -5,9 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CUT</title>
-    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="styleIndex.css" />
 </head>
-<!-- Work mobile first -->
 
 <body>
     <header>
@@ -43,33 +42,24 @@
 
     <section class="currentSection">
         <div class="current">
-
-            <!-- Currently showing:
-            Mobile:
-            2 movie images
-            Arrows in both sides, click to show other??
-            
-            Desktop:
-            3 movie images
-            Each has a title and genre-->
             <h2>Currently showing</h2>
             <button class="btnSeeAllMovies">See all</button>
             <div class="currentPosterGrid">
                 <button class="leftArrow"></button>
                 <button class="rightArrow"></button>
                 <div class="currentMoviePoster">
-                    <!-- <a href="movie.php"> -->
-                    <!-- Poster 1 -->
-                    <!-- <img class="currentPoster" src="" alt="" /> -->
-                    <!-- </a> -->
+                    <a href="movie.php">
+                        <!-- Poster 1 -->
+                        <img class="currentPoster" src="" alt="" />
+                    </a>
                 </div>
 
                 <div class="currentMoviePoster">
-                    <!-- <img class="currentPoster" src="" alt="" /> -->
+                    <img class="currentPoster" src="" alt="" />
                 </div>
 
                 <div class="currentMoviePoster">
-                    <!-- <img class="currentPoster" src="" alt="" /> -->
+                    <img class="currentPoster" src="" alt="" />
                 </div>
             </div>
 
@@ -94,17 +84,6 @@
     </section>
 
     <section class="offersSection">
-        <!-- Offers:
-    
-    Mobile:
-     2 orange boxes with 1 offer in each.
-     Next row: 1 box with button "See all"
-
-
-     Desktop:
-     1 row:
-        2 orange boxes with 1 offer in each, followed by 1 red button "See all"
-      -->
         <h2>Offers</h2>
         <div class="offersFlex">
             <div class="offerGrid">
@@ -124,15 +103,6 @@
     </section>
 
     <section class="comingSection">
-        <!-- Coming soon...
-
-    Mobile:
-     1 row with 3 mini images, arrow buttons on each side, click to show other
-
-     Desktop:
-     1 row with 5 mini images
-    -->
-
         <h2>Coming soon...</h2>
         <div class="comingGrid">
             <button class="leftArrow"></button>
@@ -179,4 +149,3 @@
 
     <?php
     require __DIR__ . "/footer.php"; ?>
-    <!-- require in footer.php -->
