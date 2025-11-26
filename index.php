a00b2e01a735acb5229b610e965595b895f0d56b
Emilie Sengenbjerg <Emilie_Sengenbjerg@hotmail.com>
2025-11-26 09:10:46 +0100

    Split CSS in 2 files

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CUT</title>
     <link rel="stylesheet" href="styleIndex.css" />
+    <link rel="stylesheet" href="styleMediaIndex.css" />
 </head>
 
 <body>
