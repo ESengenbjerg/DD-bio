3bb4db31bfac3a4449835b16cb4bdc50c2490f25
Emilie Sengenbjerg <Emilie_Sengenbjerg@hotmail.com>
2025-11-29 16:22:31 +0100

    Prettier did some unwanted changes. Debugged it

--- movie.php
+++ movie.php
@@ -158,7 +158,7 @@
 
         <section class="imgFromMovie">
             <button id="prev" class="leftArrow">
-                << /button>
+                < </button>
                     <img id="slideshow" class="imgFromMovie" src="https://images.unsplash.com/photo-1762755126280-6d8a4f9d1115?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw5fHx8ZW58MHx8fHx8&auto=format&fit=crop&q=60&w=900" alt="Slideshow with images" />
                     <button id="next" class="rightArrow">></button>
         </section>
